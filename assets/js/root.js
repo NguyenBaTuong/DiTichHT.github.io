ac283d9120dac6a046021ab234922d90c6d9abd0
NguyenBaTuong <61742750+NguyenBaTuong@users.noreply.github.com>
2021-12-26 16:55:28 +0700

    fix layout & add new festival(new page)

--- assets/js/root.js
+++ assets/js/root.js
@@ -2,6 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     var header = document.getElementById('header');
+    var content = document.getElementById('content');
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     header.style.display = 'none';
   } else {
